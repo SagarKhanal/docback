865c014d6906ca5ce7c24067a2854c2f2bd6706e
smartdrevil <jamesbrown8@my.smccd.edu>
2019-05-10 16:34:53 +0545

    Socket Updated

--- index.js
+++ index.js
@@ -1,45 +1,16 @@
-// const express = require("express");
-// const app = express();
-// const server = require("http").createServer(app);
-// const io = require("socket.io").listen(server);
-// const port = 3000;
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io").listen(server);
+const port = 3000;
 
 
-// io.on('connection',function(socket){
-//     console.log("User Connected");
-//     socket.on("Chat Message",msg=>{
-//         console.log(msg);
-//         io.emit("Chat Message",msg);
-//     })
-// });
-
-// server.listen(port,()=>console.log("Server running on port "+port));
-
-
-const express = require('express');
-const SocketServer = require('ws').Server;
-const path = require('path');
-
-const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
-
-const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
-const wss = new SocketServer({ server });
-
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on("Chat Message",msg=>{
-    console.log(msg);
-    ws.emit("Chat Message",msg)
-  })
-  ws.on('close', () => console.log('Client disconnected'));
+io.on('connection',function(socket){
+    console.log("User Connected");
+    socket.on("Chat Message",msg=>{
+        console.log(msg);
+        io.emit("Chat Message",msg);
+    })
 });
 
-setInterval(() => {
-  wss.clients.forEach((client) => {
-    client.send(new Date().toTimeString());
-  });
-}, 1000);
+server.listen(port,()=>console.log("Server running on port "+port));
